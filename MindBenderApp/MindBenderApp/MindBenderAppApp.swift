fd5d26601fe54bba5ff907d306a6bbb18edc7e97
DIRGH SHAH <dirghvshah@gmail.com>
2026-05-24 02:13:48 -0400

    Add Xcode project for MindBenderApp target

---
+++ MindBenderApp/MindBenderApp/MindBenderAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  MindBenderAppApp.swift
+//  MindBenderApp
+//
+//  Created by Dirgh Shah on 5/24/26.
+//
+
+import SwiftUI
+import App
+
+@main
+struct MindBenderAppEntry: SwiftUI.App {
+    var body: some Scene {
+        RootScene()
+    }
+}
